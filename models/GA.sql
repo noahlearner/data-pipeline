843d7f89fa7a7a875b550bcf2b60c070872bdc83
Noah Learner <noahlearner@gmail.com>
2020-03-26 17:45:15 -0600

    Update GA.sql

--- models/GA.sql
+++ models/GA.sql
@@ -1,5 +1,1 @@
-{{ config(materialized='table') }}
-
-
-Select *, DATETIME(CURRENT_TIMESTAMP() , "America/New_York") as Create_Date
-from `{{ var("client") }}.GA_in` where Month is not null
+Select * from `{{ var("client") }}.GA_in` where Month is not null
